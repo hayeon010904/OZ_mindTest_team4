218f2f03f6f040fc0e7254c26f5168e98ba39dba
sonsodockje <hanaroo0115@naver.com>
2024-01-11 14:32:13 +0900

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const questions = [
   {
-    question: "친구가 마라 붕어빵이 맛있다며 먹어보라고 한다. 나의 대답은?",
+    question: "질문",
     answer: {
-      a: { text: "오오 그래??? 나중에 먹어봐야지", type: "A" },
+      a: { text: '"<p bread-type="A">오오 그래??? 나중에 먹어봐야지</p>"' },
       b: { text: "답", type: "유형" },
     },
   },
