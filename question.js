63feba04b5f215c7708ca458f02840de92890757
김혜민 <121798650+sonsodockje@users.noreply.github.com>
2024-01-12 18:44:21 +0900

    Update question.js

--- question.js
+++ question.js
@@ -123,28 +123,6 @@ const questions = [
     },
   },
 ];
-const breadResult = [
-  {
-    breadType: "팥붕어빵",
-    comment: "이것은 팥붕어빵 입니다. ",
-  },
-  {
-    breadType: "슈붕어빵",
-    comment: "이것은 슈붕어빵 입니다. ",
-  },
-  {
-    breadType: "피자붕어빵",
-    comment: "이것은 피자붕어빵 입니다. ",
-  },
-  {
-    breadType: "녹두붕어빵",
-    comment: "이것은 녹두붕어빵 입니다. ",
-  },
-  {
-    breadType: "고구마붕어빵",
-    comment: "이것은 고구마붕어빵 입니다. ",
-  },
-];
 
 const breadResult = [
   {
