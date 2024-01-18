a1d97a79db130f3263e0af4171f00a01e8a07a31
hayeon010904 <hayeon010904@gmail.com>
2024-01-19 02:57:25 +0900

    질문,답변수정, 유형수정,6개답변나오게

--- scripts/result.js
+++ scripts/result.js
@@ -4,20 +4,24 @@ const breadResult = [
     comment: "이것은 팥붕어빵 입니다. ",
   },
   {
+    breadType: "탕후루붕어빵",
+    comment: "이것은 탕후루붕어빵 입니다. ",
+  },
+  {
     breadType: "슈붕어빵",
     comment: "이것은 슈붕어빵 입니다. ",
   },
   {
-    breadType: "피자붕어빵",
-    comment: "이것은 피자붕어빵 입니다. ",
+    breadType: "고구마붕어빵",
+    comment: "이것은 고구마붕어빵 입니다. ",
   },
   {
     breadType: "녹두붕어빵",
     comment: "이것은 녹두붕어빵 입니다. ",
   },
   {
-    breadType: "고구마붕어빵",
-    comment: "이것은 고구마붕어빵 입니다. ",
+    breadType: "피자붕어빵",
+    comment: "이것은 피자붕어빵 입니다. ",
   },
 ];
 
@@ -31,25 +35,23 @@ resultImg.attributes[1].value = `images/bread_${type}.png`;
 resultContent.textContent = breadResult[type].breadType;
 resultInfo.textContent = breadResult[type].comment;
 
-
 reloadBtn.addEventListener("click", () => {
   location.href = "index.html";
 });
 
 ////공유하기버튼...잘안됩니다
-function clip(){
-  const url = '';
+function clip() {
+  const url = "";
   const textarea = document.createElement("textarea");
   document.body.appendChild(textarea);
-  url = 'window.location.href';
+  url = "window.location.href";
   textarea.value = url;
   textarea.select();
   document.execCommand("copy");
   document.body.removeChild(textarea);
-  alert("링크 복사완료!")
-};
+  alert("링크 복사완료!");
+}
 
-shareBtn.addEventListener('click',function(){
-  clip()
-})
-
+shareBtn.addEventListener("click", function () {
+  clip();
+});
